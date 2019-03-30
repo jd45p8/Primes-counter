9d2c06e4aaab719d8d3586ccb93552f8bf4b7169
José David Polo <jd45p8@gmail.com>
2019-03-30 08:29:37 -0500

    Ya funciona hasta el 100

--- count_numbers.py
+++ count_numbers.py
@@ -1,15 +1,25 @@
+import array
 import numpy as np
 
-vec = np.zeros((1000,1))
+
+n = 1000
+vec = array.array('b',[0b0]*n)
+vec[0] = 0b1
 i = 2
-while i <= 100:
-    j = i
-    while j <= 1000:
-        vec[j-1,0] = j
+for i in [2,3,5,7]:
+    j = i*2
+    while j <= n:
+        vec[j-1] = 0b1
         j += i
     if i > 2:
-        i += 1
+        i += 2
     else:
         i += 1
 
-print(vec)
+cont = 0
+for i in range(0,n,1):
+    if(vec[i] == 1):
+        cont += 1
+    else:
+        print(i+1)
+print(n-cont)
--- count_primes.py
+++ count_primes.py
@@ -13,20 +13,9 @@ def count_primes_up_to(n):
     i = 3
     limit = ceil(sqrt(n))
 
+    # Se descartan como primos todos los múltiplos comunes entre los primeros 4 primos y el número actual
+    for j in [2,3,5,7]:
+        primes -= ccm_up_to(i,j,n)
+        j += 2
 
-    while i <= limit:
-        # Se descartan todos los números que puedan ser generados como múltiplo de otro, excepto el mismo porque 
-        # si no es primo ya debe hacer sido eliminado por ser múltiplo de algún i anterior
-        not_primes = cm_up_to(i,n) - 1
-
-        # Se descartan para eliminar los múltiplos comunes entre 2 e i porque los de 2 ya fueron eliminados
-        #not_primes -= ccm_up_to(i,2,n)
-
-        # Se descartan como primos todos los múltiplos comunes entre los primeros 4 primos y el número actual
-        for j in [2,3,5,7]:
-            not_primes -= ccm_up_to(i,j,n)
-            j += 2          
-
-        primes -= not_primes
-        i += 2
     return primes
